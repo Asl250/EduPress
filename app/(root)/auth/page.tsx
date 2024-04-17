dba43a1ad3c695461b1207d8cb76771156af4f43
Asl250 <aslrahidov250@gmail.com>
2024-04-17 16:22:58 +0500

    auth

--- app/(root)/auth/page.tsx
+++ app/(root)/auth/page.tsx
@@ -2,8 +2,6 @@
 
 import Navbar from '@/app/(root)/_components/Navbar'
 import Auth from '@/components/auth'
-// import LoginModal from '@/components/modals/login-modal'
-import useLoginModal from '@/hooks/useLoginModals'
 import { authOptions } from '@/lib/auth-options'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
