2177096c2c790b61b07aa088f9eaee0cefdadd81
Asl250 <aslrahidov250@gmail.com>
2024-04-17 17:05:41 +0500

    auth

--- app/(root)/auth/page.tsx
+++ app/(root)/auth/page.tsx
@@ -1,11 +1,9 @@
-"use client"
 
 import Navbar from '@/app/(root)/_components/Navbar'
 import Auth from '@/components/auth'
 import { authOptions } from '@/lib/auth-options'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
-import React, { useCallback } from 'react'
 
 export default async function AuthPage(){
 	const session: any = await getServerSession(authOptions);
